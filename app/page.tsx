b3d5bf3f7ecbe9c5ba0e58a4f5ca80e1dcfbc081
Ben Everman <94769250+beverm2391@users.noreply.github.com>
2024-07-05 15:37:07 -0400

    gave up on the card stack

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,11 @@
 import AirbnbImageAnimation from "@/core/components/AirbnbImageAnimation";
 import { FileTree } from "@/core/components/FileTree";
-import { SwipeableCardStackDemo } from "@/core/components/SwipeableCardStack";
 
 
 export default function Home() {
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       hi
-      <SwipeableCardStackDemo />
     </div>
   );
 }
