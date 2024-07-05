e3450bdc71f75792ce2661bb5a2093a8c757ab32
Ben Everman <94769250+beverm2391@users.noreply.github.com>
2024-07-05 15:05:38 -0400

    fix import errors

--- core/components/shadcn-ui/toggle-group.tsx
+++ core/components/shadcn-ui/toggle-group.tsx
@@ -5,7 +5,7 @@ import * as ToggleGroupPrimitive from "@radix-ui/react-toggle-group"
 import { VariantProps } from "class-variance-authority"
 
 import { cn } from "@/core/lib/utils"
-import { toggleVariants } from "@/components/ui/toggle"
+import { toggleVariants } from "@/core/components/shadcn-ui/toggle"
 
 const ToggleGroupContext = React.createContext<
   VariantProps<typeof toggleVariants>
