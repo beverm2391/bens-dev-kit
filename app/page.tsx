53cef9a4f12c061b72875ebcd5d16602504d7cd4
Ben Everman <94769250+beverm2391@users.noreply.github.com>
2024-07-17 16:12:34 -0400

    added tabs clip path

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 import AirbnbImageAnimation from "@/core/components/AirbnbImageAnimation";
 import { FileTree } from "@/core/components/FileTree";
+import TabsClipPath from "@/core/components/TabsClipPath";
+import { Tabs } from "@radix-ui/react-tabs";
 
 
 export default function Home() {
   return (
     <div className="flex-1 w-full flex flex-col gap-20 items-center">
       hi
+      <TabsClipPath />
     </div>
   );
 }
